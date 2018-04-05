4984de5f90912cf59b7805cae1a974815a5d0078
Willian Z <Willian@Willian-Zhang.com>
2018-04-05 18:54:26 -0400

    complete speed

--- Device.js
+++ Device.js
@@ -1,35 +1,38 @@
 import './libraries/eventemitter2.js';
 
 export class Piano extends EventEmitter2{
-    constructor(win){
+    constructor(win, speed_dectection_duration = 5000){
         super();
         this.window = win ? win : window;
         this.no_device_alert = null;
+        this.pressing = new Set();
+        // Speed
+        this.speed = 0;
+        this.speed_dectection_duration = speed_dectection_duration;
     }
     connect(){
-        let self = this;
         this.window.navigator.requestMIDIAccess()
-        .then(function(access) {
+        .then((access) =>{
             // Get lists of available MIDI controllers
             const inputs = access.inputs.values();
             const outputs = access.outputs.values();
             let device = [...inputs][0];
 
-            access.onstatechange = function(e) {
+            access.onstatechange = (e) => {
                 // Print information about the (dis)connected MIDI controller
                 if(e.port instanceof MIDIInput && e.port.state == 'connected'){
-                    self.deviceConnected(e.port)
+                    this.deviceConnected(e.port)
                 }
                 if(e.port.state == 'disconnected'){
                     if([...access.inputs.values()].length == 0){
-                        self.no_device_alert = swal("No device found", "Waiting for connection...", "info");
+                        this.no_device_alert = swal("No device found", "Waiting for connection...", "info");
                     }
                 }
             };
             if(device){
-                self.deviceConnected(device);
+                this.deviceConnected(device);
             }else{
-                self.no_device_alert = swal("No device found", "Waiting for connection...", "info");
+                this.no_device_alert = swal("No device found", "Waiting for connection...", "info");
             }
         });
     }
@@ -39,19 +42,34 @@ export class Piano extends EventEmitter2{
             swal.close();
         }
         
-        let self = this;
-        device.onmidimessage = function(message){
+        device.onmidimessage = (message) => {
+            let key = message.data[1];
             switch (message.data[0]) {
                 case 144:
-                    self.emit('press', message.data[1]);
+                    this.emit('press', key);
+                    this.pressing.add(key);
                     break;
                 case 128:
-                    self.emit('release', message.data[1]);
+                    this.emit('release', key);
+                    this.pressing.delete(key);
                     break;
                 default:
                     break;
             }
         }
-        
+    }
+    detectSpeed(speed_dectection_duration = null){
+        let timeout = speed_dectection_duration ? speed_dectection_duration : this.speed_dectection_duration;
+
+        let increment = ()=>{
+            this.speed += 1;
+            this.window.setTimeout(() => {
+                this.speed -= 1;
+                this.emit('speed', this.speed);
+            }, timeout);
+            this.emit('speed', this.speed);
+        }
+        this.on('press', increment);
+        this.on('release', increment);
     }
 }
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import {Piano} from './Device.js';
 let piano = new Piano(window);
 
 piano.connect();
-
-piano.on('press', console.log)
+piano.detectSpeed();
+piano.on('speed', console.log)
